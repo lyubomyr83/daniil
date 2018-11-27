1efb1c308e47a3bcb60f3fd297bba1498665b00e
ilya682003 <ilyanfs682003@mail.ru>
2018-11-27 17:03:16 +0300

    модификаторы доступа, статические свойства и методы, позднее статическое связывание

--- Car.php
+++ Car.php
@@ -2,31 +2,21 @@
 
 class Car
 {
-    public $car_material;
-    public $car_color;
-    public $windows;
-    public $doors;
-    public $car_speed;
-    public $car_condition;
-    public $car_cost;
-    public $car_year;
+    protected $type = "Автомобиль";
+    protected $brand;
+    protected $car_material;
+    protected $car_color;
+    protected $windows;
+    protected $doors;
+    protected $car_speed;
+    protected $car_condition;
+    protected $car_cost;
+    protected $car_year;
+    protected static $col; // количество
 
-    public function getInfo()
+    public function __construct($br,$cc,$cm,$w,$d,$cs,$c_con,$cco,$cy)
     {
-        echo "Цвет машины: ".$this->car_color."<br>";
-        echo "Материал машины: ".$this->car_material."<br>";
-        echo "Окна: ".$this->windows."<br>";
-        echo "Двери: ".$this->doors."<br>";
-        echo "Скорость машины: ".$this->car_speed."<br>";
-        echo "Состояние машины: ".$this->car_condition."<br>";
-        echo "Скорость машины: ".$this->car_cost."<br>";
-        echo "Год выпуска: ".$this->car_year."<br>";
-    }
-}
-class superHome extends Car
-{
-    public function __construct($cc,$cm,$w,$d,$cs,$c_con,$cco,$cy)
-    {
+        $this->brand = $br;
         $this->car_color = $cc;
         $this->car_material = $cm;
         $this->window = $w;
@@ -36,11 +26,71 @@ class superHome extends Car
         $this->car_cost = $cco;
         $this->car_year = $cy;
 
-        $this->getInfo();
+        // считаем количество автомобилей
+        static::$col ++;
+    }
+
+    public function getInfo()
+    {
+        echo $this->type."<br>";
+        echo "Марка: ".$this->brand."<br>";
+        echo "Цвет машины: ".$this->car_color."<br>";
+        echo "Материал машины: ".$this->car_material."<br>";
+        echo "Окна: ".$this->windows."<br>";
+        echo "Двери: ".$this->doors."<br>";
+        echo "Скорость машины: ".$this->car_speed."<br>";
+        echo "Состояние машины: ".$this->car_condition."<br>";
+        echo "Скорость машины: ".$this->car_cost."<br>";
+        echo "Год выпуска: ".$this->car_year."<br>";
+        echo "<br>";
+    }
+
+    public static function getCol()
+    {
+        echo static::$col;
+        echo "<br>";
+    }
+}
+
+class Bus extends Car
+{
+    protected $type = "Автобус";
+    protected $bus_seats;
+    protected static $col; // количество
+
+    public function __construct($st,$br, $cc, $cm, $w, $d, $cs, $c_con, $cco, $cy)
+    {
+        parent::__construct($br, $cc, $cm, $w, $d, $cs, $c_con, $cco, $cy);
+        $this->bus_seats = $st;
+
+    }
+
+    public function getInfo()
+    {
+        echo "Количество мест: ".$this->bus_seats."<br>";
+        parent::getInfo();
+
     }
 
 
 }
 
-$myCar = new superHome("Черный","Железо",6,2,230,"Новая",1500000,2004);
-$friendCar = new superHome("Голубой","Бетон",6,4,200,"Разбитая",1200000,1999);
+class Truck extends Car
+{
+    protected $type = "Грузовик";
+    protected $truck_cargo;
+    protected static $col; // количество
+}
+
+$myCar = new Car("Ford","Черный","Железо",6,2,230,"Новая",1500000,2004);
+$myCar->getInfo();
+$friendCar = new Bus(18,"Mercedes","Голубой","Бетон",6,4,200,"Разбитая",1200000,1999);
+$friendCar->getInfo();
+$friendCar2 = new Bus(60,"Mercedes","Голубой","Бетон",6,4,200,"Разбитая",1200000,1999);
+$friendCar2->getInfo();
+$friendCar3 = new Truck("Камаз","Голубой","Бетон",6,4,200,"Разбитая",1200000,1999);
+$friendCar3->getInfo();
+
+Car::getCol();
+Bus::getCol();
+Truck::getCol();
